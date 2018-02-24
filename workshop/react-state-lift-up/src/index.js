d3786e319dc680e9c322384e4c4e523460adfe62
Arul Prasad <arul@sella.it>
2018-02-25 04:59:04 +0530

    code refactored

--- workshop/react-state-lift-up/src/index.js
+++ workshop/react-state-lift-up/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import {SearchApp} from './SearchApp';
 import registerServiceWorker from './registerServiceWorker';
 
