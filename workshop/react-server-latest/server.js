cbe7ea6db617dabe5527a20a0ce95e279c6cbbfe
Arul Prasad <arul@sella.it>
2018-03-03 14:40:58 +0530

    first version server dev

--- workshop/react-server-latest/server.js
+++ workshop/react-server-latest/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
@@ -24,7 +24,7 @@ app.get('/experts', function(req,res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
